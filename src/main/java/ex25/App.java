547b2fe0d005f135545fcabbf525fd8aed4195c7
polvnco <cpolanco000@gmail.com>
2021-06-10 20:32:16 -0400

    finished, could be polished

--- src/main/java/ex25/App.java
+++ src/main/java/ex25/App.java
@@ -5,42 +5,39 @@
 package ex25;
 
 
-//import java.util.Scanner;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class App {
     public static void main(String[] args) {
-
-        int pass_strength = passwordValidator();
+        passwordValidator();
 
     }
 
-    public static int passwordValidator() {
+    public static void passwordValidator() {
 
         String password = callParser();
 
         boolean very_weakPassword = vwPassword(password);
         boolean weakPassword = wPassword(password);
         boolean strongPassword = sPassword(password);
-        //..strong password
-        //..very strong password
+        boolean very_strongPassword = vsPassword(password);
 
         if (very_weakPassword) {
             System.out.print("The password " + password + " is a very weak password");
-            return 1;
         }
         else if (weakPassword) {
             System.out.print("The password " + password + " is a weak password");
-            return 1;
         }
-        else if (strongPassword) {
+        else if (strongPassword && !very_strongPassword) {
             System.out.print("The password " + password + " is a strong password");
-            return 1;
-        } else {
-            return 0;
         }
+        else if (very_strongPassword) {
+            System.out.print("The password " + password + " is a very strong password");
         }
+    }
 
     public static String callParser() {
         System.out.print("ENTER A PASSWORD: ");
@@ -55,13 +52,18 @@ public class App {
         return pass.length() < 8 && char_check(pass);
     }
     public static boolean sPassword(String pass) {
-        return pass.length() >= 8 && (int_check(pass) && char_check(pass));
+        if (pass.length() >= 8 && char_check(pass))
+        {
+            return int_check(pass);
+        }
+        return true;
+    }
+    public static boolean vsPassword(String pass) {
+        return isSpecial(pass);
     }
 
+
     public static boolean int_check(String pass) {
-        //compare character by character to integer value
-        //if character is int boolean = true
-        //if non int found boolean = false
         char c;
         for (int i = 0; i < pass.length(); i++) {
             c = pass.charAt(i);
@@ -72,9 +74,6 @@ public class App {
         return true;
     }
     public static boolean char_check(String pass) {
-        //compare character by character to integer value
-        //if character is int boolean = true
-        //if non int found boolean = false
         char c;
         for (int i = 0; i < pass.length(); i++) {
             c = pass.charAt(i);
@@ -83,5 +82,12 @@ public class App {
             }
         }
         return true;
+    }
+
+    public static boolean isSpecial(String pass){
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9]");
+        Matcher matcher = pattern.matcher(pass);
+        return matcher.find();
+
     }
 }
