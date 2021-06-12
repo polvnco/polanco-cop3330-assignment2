6ab8c5fde6096357317f85f6be2c46f133c68d68
polvnco <cpolanco000@gmail.com>
2021-06-11 21:43:00 -0400

    fixed the spacing in the output

--- src/main/java/ex30/App.java
+++ src/main/java/ex30/App.java
@@ -19,7 +19,7 @@ public class App {
 
         for (int i = 1; i <= tableGenerator; i++) {
             for (int j = 1; j <= tableGenerator; j++) {
-                System.out.format("%4d", i * j);
+                System.out.format("%5d", i * j);
             }
             System.out.println();
         }
