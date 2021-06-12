493ad3052f79253392c0b22a65081ae8c4a4b2dc
polvnco <cpolanco000@gmail.com>
2021-06-12 13:21:01 -0400

    added challenge of error input

---
+++ src/main/java/ex34/App.java
@@ -0,0 +1,38 @@
+package ex34;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        App employ = new App();
+        employ.currentEmployees();
+    }
+
+    private void currentEmployees(){
+        var emp = new ArrayList<>(List.of("John Smith", "Jackie Jackson",
+                "Chris Jones", "Amanda Cullen", "Jeremy Goodwin"));
+        printEmployees(emp);
+        if (!deleteEmployees(emp)){
+            System.out.println("Entry does not exist");
+        }
+        else{
+            System.out.println();
+            printEmployees(emp);
+        }
+    }
+
+    private void printEmployees(List<String> emp){
+        System.out.printf("There are %d employees:%n", emp.size());
+        emp.forEach(System.out::println);
+    }
+
+    private boolean deleteEmployees(List<String> emp){
+        System.out.println();
+        System.out.print("Enter an employee to remove: ");
+        Scanner removeEmployee = new Scanner(System.in);
+        var employee = removeEmployee.nextLine();
+        return emp.remove(employee);
+    }
+}
