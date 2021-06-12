eff403051606c804714b4ce8922be6308825e831
polvnco <cpolanco000@gmail.com>
2021-06-11 21:30:05 -0400

    added an exception

---
+++ src/main/java/ex30/App.java
@@ -0,0 +1,30 @@
+package ex30;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        App object = new App();
+        object.multiplicationTable();
+    }
+
+    public void multiplicationTable() {
+        Scanner tableSize = new Scanner(System.in);
+        int tableGenerator = tableSize.nextInt();
+        if (tableGenerator > 12 || tableGenerator < 1){
+            System.out.print("Please enter a number between 1 - 12.");
+            return;
+        }
+        for (int i = 1; i <= tableGenerator; i++) {
+            System.out.format("%4d", i);
+        }
+        System.out.println();
+
+        for (int i = 2; i <= tableGenerator; i++) {
+            for (int j = 1; j <= tableGenerator; j++) {
+                System.out.format("%4d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
