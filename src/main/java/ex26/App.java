7edad7b61797603a1b4e43c4a6b2fec4e89e2945
polvnco <cpolanco000@gmail.com>
2021-06-13 18:56:51 -0400

    line 37 missing void and calling the object did nothing with what it returned originally

--- src/main/java/ex26/App.java
+++ src/main/java/ex26/App.java
@@ -11,39 +11,40 @@ public class App {
 
     public static void main(String[] args) {
 
-        int numMonths = (int) PaymentCalculator.calculateMonthsUntilPaidOff();
-        System.out.println("It will take you " + numMonths + " months to pay off this card.");
+        App apr = new App();
+        apr.calculateMonthsUntilPaidOff();
+
     }
 
-    public static double callBalance() {
+    public double callBalance() {
         Scanner balance = new Scanner(System.in);
         System.out.print("What is your balance? ");
         return balance.nextDouble();
     }
 
-    public static double callInterestRate() {
+    public double callInterestRate() {
         Scanner interest = new Scanner(System.in);
         System.out.print("What is the APR on the card (as a percent)? ");
         return interest.nextDouble();
     }
-    public static double callPayment() {
+
+    public double callPayment() {
         Scanner payment = new Scanner(System.in);
         System.out.print("What is the monthly payment you can make? ");
         return payment.nextDouble();
     }
 
-    public static class PaymentCalculator{
-        public static double calculateMonthsUntilPaidOff(){
-            double b = callBalance();
-            double i = callInterestRate() / 365 / 100;
-            double p = callPayment();
-            double calc1 = Math.pow((1 + i), 30);
-            double calc2 = 1 - calc1;
-            double calc3 = calc2 * (b / p);
-            double calc4 = 1 + calc3;
-            double calc5 = Math.log(calc4) / Math.log(1 + i);
-            return Math.ceil((-1.00 / 30) * calc5);
-        }
+    public void calculateMonthsUntilPaidOff() {
+        var b = callBalance();
+        var i = callInterestRate() / 365 / 100;
+        var p = callPayment();
+        var calc1 = Math.pow((1 + i), 30);
+        var calc2 = 1 - calc1;
+        var calc3 = calc2 * (b / p);
+        var calc4 = 1 + calc3;
+        var calc5 = Math.log(calc4) / Math.log(1 + i);
+        var calc6 = Math.ceil((-1.00 / 30) * calc5);
+        System.out.printf("It will take you %d months to pay off this card.", (int) calc6);
     }
 }
 
