b001de7fb60309206416ce344af5dab6249b2608
polvnco <cpolanco000@gmail.com>
2021-06-13 18:37:40 -0400

    added variable for #ofContestants

--- src/main/java/ex35/App.java
+++ src/main/java/ex35/App.java
@@ -11,15 +11,17 @@ public class App {
     public static void main(String[] args) {
         App winner = new App();
         winner.printWinner();
-
     }
     public ArrayList<String> getList(){
+        System.out.print("Enter the amount of contestants: ");
+        Scanner contestantsAmount = new Scanner(System.in);
+        int cAmount = contestantsAmount.nextInt();
         var people = new ArrayList<String>();
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < cAmount; i++){
             System.out.print("Enter a name: ");
             Scanner person = new Scanner(System.in);
             var name = person.nextLine();
-            if (name.matches(" ") || name.length() == 0){
+            if (name.matches(" ") || (name.length() == 0)){
                 break;
             }
             people.add(name);
@@ -31,13 +33,15 @@ public class App {
     public void printWinner(){
         var people = getList();
         if (people.size() > 0){
-            var prize = chooseWinner(people);
-            System.out.printf("The winner is...%s.%n", prize);
+            String prize;
+            prize = chooseWinner(people);
+            System.out.printf("The winner is... %s.%n", prize);
         }
     }
 
     public String chooseWinner(List<String> people){
-        int ofList = random.nextInt(people.size());
+        int ofList;
+        ofList = random.nextInt(people.size());
         return people.get(ofList);
     }
 }
