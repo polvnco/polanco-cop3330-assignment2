660bf4b2cfc61c07b475665602bbf793c1fc0840
polvnco <cpolanco000@gmail.com>
2021-06-13 21:36:25 -0400

    divided it up into four tests

--- src/test/java/ex25/passwordValidate.java
+++ src/test/java/ex25/passwordValidate.java
@@ -1,3 +1,7 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Christopher Polanco
+ */
 package ex25;
 
 import org.junit.jupiter.api.Test;
@@ -7,21 +11,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class passwordValidate {
 
     @Test
-    void passwordValidator() {
+    void test_If_Very_Strong_Password_Entered() {
         App veryStrongTest = new App();
-        App strongTest = new App();
-        App weakTest = new App();
-        App veryWeakTest = new App();
 
         boolean vs = veryStrongTest.vsPassword("abcd1234$");
-        boolean s = strongTest.sPassword("abcd1234");
-        boolean w = weakTest.wPassword("abcd");
-        boolean vw = veryWeakTest.vwPassword("1234");
 
 
         assertTrue(vs);
+    }
+    @Test
+    void test_If_Strong_Password_Entered() {
+        App strongTest = new App();
+
+        boolean s = strongTest.sPassword("abcd1234");
+
+
         assertTrue(s);
+    }
+    @Test
+    void test_If_Weak_Password_Entered() {
+        App weakTest = new App();
+
+        boolean w = weakTest.wPassword("abcd");
+
+
         assertTrue(w);
+    }
+    @Test
+    void test_If_Very_Weak_Password_Entered() {
+        App veryWeakTest = new App();
+
+        boolean vw = veryWeakTest.vwPassword("1234");
+
+
         assertTrue(vw);
     }
 }
