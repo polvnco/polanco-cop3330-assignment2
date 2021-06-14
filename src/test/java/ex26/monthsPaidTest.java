f00be4d463658e9e5c6f7cb80c9e7a1c69112172
polvnco <cpolanco000@gmail.com>
2021-06-13 20:23:21 -0400

    im still unsure if this is a good enough test

---
+++ src/test/java/ex26/monthsPaidTest.java
@@ -0,0 +1,26 @@
+package ex26;
+
+import org.junit.jupiter.api.Test;
+
+class monthsPaidTest {
+
+    @Test
+    void callBalance() {
+        App app = new App();
+        app.setBalance(1000);
+        assert app.getBalance() == 1000;
+    }
+    @Test
+    void callInterestRate() {
+        App app = new App();
+        app.setInterestRate(12);
+        assert app.getInterestRate() == 12;
+    }
+
+    @Test
+    void callPayment() {
+        App app = new App();
+        app.setPayment(100);
+        assert app.getPayment() == 100;
+    }
+}
